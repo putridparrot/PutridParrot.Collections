f10a3dccd9f2be18098eb4410103056b698a6d38
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-08-05 21:29:16 +0100

    Added BinaryTreeNode and further traversal code

---
+++ PutridParrot.Collections/BinaryTreeNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PutridParrot.Collections
+{
+    public class BinaryTreeNode<T>
+    {
+        public BinaryTreeNode()
+        {
+        }
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public static implicit operator T(BinaryTreeNode<T> node)
+        {
+            return node.Value;
+        }
+
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+    }
+
+
+}
--- PutridParrot.Collections/TreeNodeExtensions.cs
+++ PutridParrot.Collections/TreeNodeExtensions.cs
@@ -18,5 +18,26 @@ namespace PutridParrot.Collections
         {
             TraverseTree.BreadthTraversal(node, n => n.ChildNodes, action);
         }
+
+        public static void PreOrderTraversal<T>(this BinaryTreeNode<T> node, Action<BinaryTreeNode<T>> action)
+        {
+            TraverseTree.PreOrderTraversal(node, n => new [] { n.Left, n.Right }, action);
+        }
+
+        public static void PostOrderTraversal<T>(this BinaryTreeNode<T> node, Action<BinaryTreeNode<T>> action)
+        {
+            TraverseTree.PostOrderTraversal(node, n => new[] { n.Left, n.Right }, action);
+        }
+
+        public static void InOrderTraversal<T>(this BinaryTreeNode<T> node, Action<BinaryTreeNode<T>> action)
+        {
+            TraverseTree.InOrderTraversal(node, n => new[] { n.Left, n.Right }, action);
+        }
+
+        public static void BreadthTraversal<T>(this BinaryTreeNode<T> node, Action<BinaryTreeNode<T>> action)
+        {
+            TraverseTree.BreadthTraversal(node, n => new[] { n.Left, n.Right }, action);
+        }
+
     }
 }
