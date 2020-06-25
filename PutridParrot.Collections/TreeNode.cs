fae13bc8f26e7c47cd4e5784f87490825c0c8ffe
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-06-25 20:22:13 +0100

    Converted to .net core 3.1

--- PutridParrot.Collections/TreeNode.cs
+++ PutridParrot.Collections/TreeNode.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace PutridParrot.Collections
 {
